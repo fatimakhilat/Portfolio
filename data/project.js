0b38ea1832bda5a9bf0a4b151e4d2e44ef54c5fa
Fatima <fatimakhilat3@gmail.com>
2026-08-08 00:28:54 +0100

    File containing all the card information, plus the function that displays the cards.

---
+++ data/project.js
@@ -0,0 +1,653 @@
+
+
+export const projects = [
+  {
+    id: 1,
+    category: "frontend",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal, responsive portfolio website.", technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 2,
+    category: "backend",
+    image: {
+      src: "images/projet2_landingPage.jpg",
+      alt: "Landing Page"
+    },
+    logo: {
+      src: "icons/rocket.png",
+      alt: "Rocket icon"
+    },
+    title: "Landing Page",
+    description: "Modern landing page for an agency.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Animations", "SEO"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 3,
+    category: "frontend",
+    image: {
+      src: "images/projet3_Dashboard.jpg",
+      alt: "Dashboard Application"
+    },
+    logo: {
+      src: "icons/chart.png",
+      alt: "Chart icon"
+    },
+    title: "Dashboard",
+    description: "Simple dashboard with statistics.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Charts", "Responsive", "Dark Mode"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 4,
+    category: "cloud",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/chat.png",
+      alt: "Chat icon"
+    },
+    title: "Dashboard",
+    description: "Simple dashboard with statistics.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Cart", "Auth", "Payments"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 5,
+    category: "springboot",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 6,
+    category: "springboot",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 7,
+    category: "fullstack",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 8,
+    category: "fullstack",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 9,
+    category: "react",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 10,
+    category: "cloud",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 11,
+    category: "frontend",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 12,
+    category: "backend",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 13,
+    category: "fullstack",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 14,
+    category: "react",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 15,
+    category: "frontend",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 16,
+    category: "backend",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 17,
+    category: "fullstack",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 18,
+    category: "react",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  },
+  {
+    id: 19,
+    category: "frontend",
+    image: {
+      src: "images/projet1_portfolio.jpg",
+      alt: "Portfolio Website"
+    },
+    logo: {
+      src: "icons/user.png",
+      alt: "Portfolio icon"
+    },
+    title: "Portfolio Website",
+    description: "A Personal,responsive portfolio website.",
+    technologies: [
+      {
+        src: "images/html.svg.png",
+        alt: "HTML"
+      },
+      {
+        src: "images/css.png",
+        alt: "CSS"
+      },
+      {
+        src: "images/javascript.png",
+        alt: "JavaScript"
+      }],
+    features: ["Responsive", "Dark Mode", "Animations"],
+    github: "https://github.com/fatimakhilat",
+    live: "https://fatimakhilat.github.io/portfolio/"
+
+  }
+];
+
+
+
+
+export function showCard(project) {
+
+  const technologiesHTML = project.technologies.map(technology => {
+    return `
+      <li class="tech-tag">
+        <img src="${technology.src}" alt="${technology.alt}">
+      </li>
+    `;
+  }).join("");
+
+  const featuresHTML = project.features.map(feature => {
+    return `
+    <li class="des-tag">
+    ✓ ${feature}
+    </li>
+    `
+  }).join("");
+
+  const html = `
+    <div class="project-card js-project-card" data-category="${project.category}">
+    <div class="project-img">
+      <img src="${project.image.src}" alt="${project.image.alt}" class="projet-image">
+    </div>
+    <div class="card-container">
+      <div class="card-img">
+        <img src="${project.logo.src}"  alt="${project.logo.alt}">
+      </div>
+      <div class="card-tech">
+        <div class="project-content">
+          <h3 class="par1">${project.title}</h3>
+          <p class="par2">
+          ${project.description}
+          </p>
+        </div>
+        <ul class="project-technologies">
+        ${technologiesHTML}
+      </ul>
+      </div>
+    </div>
+    
+      <ul class="project-des">
+         ${featuresHTML}
+      </ul>
+
+    <div class="project-links">
+      <a href="${project.github}" target="_blank" rel="noopener noreferrer" class="btn-github">
+        <img src="icons/github.svg" alt="GitHub" class="img-github">
+        <span>GitHub</span>
+      </a>
+      <a href="${project.live}" target="_blank" rel="noopener noreferrer"
+        class="btn-live">🌐 Live Demo </a>
+
+    </div>
+  </div>
+    `;
+  return html;
+}
