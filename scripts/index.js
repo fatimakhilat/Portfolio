c6777c504207d1a900037ac1c4e6eb2d44284300
Fatima <fatimakhilat3@gmail.com>
2026-08-07 23:46:29 +0100

    delete dom from index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,29 +5,6 @@ import { initNavigation } from "./navigation.js";
 import { initTheme } from "./theme.js";
 
 
-
-
-/*=========================================================
-=                     DOM ELEMENTS                        =
-=========================================================*/
-
-const menuBtn = document.querySelector(".js-menu-btn");
-const menu = document.querySelector(".js-right-item");
-
-const themeBtn = document.querySelector(".js-moon-item");
-const themeIcon = document.querySelector(".js-moon-item img");
-
-const backTopBtn = document.querySelector(".js-back-top");
-
-const sections = document.querySelectorAll("section, footer");
-const navLinks = document.querySelectorAll("nav a");
-const animatedElements = document.querySelectorAll(
-  "section, footer, .skill-item, .project-card"
-);
-
-
-
-
 initMenu();
 
 initAnimations();
