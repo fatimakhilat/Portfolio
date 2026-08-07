18cbd08f19648a53e69ab9ed9dcf1fa5b58d7086
Fatima <fatimakhilat3@gmail.com>
2026-08-07 22:50:54 +0100

    create file backTop.js and create function that handles button back to top

---
+++ scripts/backTop.js
@@ -0,0 +1,43 @@
+/*=========================================================
+=                    BACK TO TOP                          =
+=========================================================*/
+
+function initBackTop() {
+
+  const backTopBtn = document.querySelector(".js-back-top");
+
+  if (!backTopBtn) {
+    return;
+  }
+
+  function toggleBackTopButton() {
+
+    backTopBtn.classList.toggle(
+      "show",
+      window.scrollY > 300
+    );
+
+  }
+
+  function scrollToTop() {
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+
+  }
+
+  window.addEventListener(
+    "scroll",
+    toggleBackTopButton
+  );
+
+  backTopBtn.addEventListener(
+    "click",
+    scrollToTop
+  );
+
+}
+
+export { initBackTop };
